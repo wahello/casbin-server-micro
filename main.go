a4aa6d0707b77a4e6eb2b32a8d7519532f48cd2c
Vasiliy Tolstov <v.tolstov@unistack.org>
2019-07-23 00:45:27 +0300

    first adaptation to go-micro
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- main.go
+++ main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate protoc -I proto --go_out=plugins=grpc:proto proto/casbin.proto
+//go:generate protoc -I proto --micro_out=proto --go_out=proto proto/casbin.proto
 
 package main
 
