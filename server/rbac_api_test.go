c9fecf78b514431f7aee78e421508dfb8133241e
Vasiliy Tolstov <v.tolstov@unistack.org>
2019-08-11 01:46:09 +0300

    disable original tests
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- server/rbac_api_test.go
+++ server/rbac_api_test.go
@@ -1,3 +1,4 @@
+// +build ignore
 // Copyright 2018 The casbin Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,8 +19,8 @@ import (
 	"testing"
 
 	"github.com/casbin/casbin/v2/util"
-	"github.com/stretchr/testify/assert"
 	casbinpb "github.com/paysuper/casbin-server/casbinpb"
+	"github.com/stretchr/testify/assert"
 )
 
 func testGetRoles(t *testing.T, e *testEngine, name string, res []string) {
